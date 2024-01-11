4aa83bb4f8f87f6b05b62eb652c5bc702c4d1a97
Payam Mehrpooya <payammehr2@gmail.com>
2024-01-11 01:37:16 +0000

    Completed lab1 SQL Queries

--- lab-1-3.sql
+++ lab-1-3.sql
@@ -1,7 +1,9 @@
 -- Which three teams had the losingest seasons?
 
 -- Expected result:
---
+SELECT year, name, wins, losses FROM teams 
+ORDER BY losses DESC
+LIMIT 3;
 -- +------+----------------------+------+--------+
 -- | 1899 | Cleveland Spiders    | 20   | 134    |
 -- | 1962 | New York Mets.       | 40   | 120    |
