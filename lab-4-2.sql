d4589119e2c3b8753be7973a7bb9be2469aceb89
Payam Mehrpooya <payammehr2@gmail.com>
2024-01-16 02:06:31 +0000

    Completed lab 4 excersizes

--- lab-4-2.sql
+++ lab-4-2.sql
@@ -5,6 +5,13 @@
 -- WHERE column1 = expression1
 -- AND column2 = expression2
 
+SELECT players.first_name, players.last_name FROM stats
+INNER JOIN players ON players.id = stats.player_id 
+INNER JOIN teams ON teams.id = stats.team_id
+WHERE teams.year = "2020"
+AND teams.name = "Chicago Cubs"; 
+
+
 -- Expected result: 47 rows starting with
 --
 -- +------------+-----------+
--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,5 +1,13 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
 
+SELECT players.first_name, players.last_name, teams.name, MAX(stats.home_runs) FROM stats
+INNER JOIN players ON players.id = stats.player_id 
+INNER JOIN teams ON teams.id = stats.team_id
+WHERE teams.year = "2019"
+GROUP BY teams.name
+ORDER BY MAX(stats.home_runs) DESC
+LIMIT 1;
+
 -- Expected result:
 --
 -- +---------------+------------+-----------+-----------+
