095d34c700ce0b3362fd8aad09321500ac947ed1
Payam Mehrpooya <payammehr2@gmail.com>
2024-01-23 23:41:12 +0000

    Class Follow Alongs

--- school-1-create.sql
+++ school-1-create.sql
@@ -3,5 +3,5 @@
 CREATE TABLE students (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
-  last_name TEXT
+  last_name TEXT,
 );
---
+++ school-5-insert.sql
@@ -0,0 +1,2 @@
+INSERT INTO students (first_name, last_name, email)
+VALUES ("BEN", "Block", "ben@kellogg.northwester.edu");
